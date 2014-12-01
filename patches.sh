2c5b44f09b5c525c591b780e56ddedf86d8ab37f
Arslan Kirmani <arslankirmani9600@gmail.com>
2014-12-01 01:07:03 -0800

    Added more patches

--- patches.sh
+++ patches.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 if [ -f ~/bin/paths-12.0.sh ]; then
-	source ~/bin/paths-12.0.sh
+    source ~/bin/paths-12.0.sh
+fi
+
+if [ ! -d ".repo" ]; then
+    echo -e "No .repo directory found.  Is this an Android build tree?"
+    exit 1
 fi
 
 if [ "${android}" = "" ]; then
-	android=~/android/system
+    android="${PWD}"
 fi
 
 # frameworks/av : Add back missing msm7x27a to fix compilation
@@ -16,5 +21,23 @@ cherries+=(79581)
 
 # frameworks/opt/telephony : UiccController: Query GET_SIM_STATUS when radio state is ON
 cherries+=(79187)
+
+# Implement fakeiccid for legacy rils (1/2)
+cherries+=(79835)
+
+# CardStateMonitor: Use fakeiccid if needed for legacy ril (2/2)
+cherries+=(79835)
+
+# Legacy RIL requires radio technology equals CDMA or GSM
+cherries+=(79912)
+
+# recovery: Always include ext4 stuff
+cherries+=(79803)
+
+# DCTracker: HACK Fix eHRPD/LTE data connection.
+cherries+=(79749)
+
+# Telephony: DcTracker: Fix CDMA APN Data issues.
+cherries+=(79764)
 
 ${android}/build/tools/repopick.py -b ${cherries[@]}
